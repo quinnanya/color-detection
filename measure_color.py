ac9d44d769213c8797f1b122a6dad936ec14f25d
Quinn Dombrowski <712016+quinnanya@users.noreply.github.com>
2021-11-16 12:18:45 -0800

    Example image and Python scripts

---
+++ measure_color.py
@@ -0,0 +1,55 @@
+# USAGE
+# python detect_color.py --image pokemon_games.png
+
+# import the necessary packages
+import streamlit as st
+import pandas as pd
+import numpy as np
+import argparse
+import cv2
+
+st.title('Color detection')
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
+
+# load the image
+image = cv2.imread(args["image"])
+dimensions = image.shape
+
+# define the list of boundaries
+# Don't forget BGR
+boundaries = [
+#Dark, light for red on first line
+#https://imagecolorpicker.com/
+	([66, 100, 200], [150, 180, 220]),
+	#Blue: dark and so light it gets mixed up with gray
+	#([100, 51, 112], [192, 190, 191]),
+	#Blue: dark and lighter but still blue
+	#([100, 51, 112], [183, 169, 154]),
+	#([25, 146, 190], [62, 174, 250]),
+	#([103, 86, 65], [145, 133, 128])
+]
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+
+	# find the colors within the specified boundaries and apply
+	# the mask
+	mask = cv2.inRange(image, lower, upper)
+
+colored = cv2.countNonZero(mask)
+print(colored)
+totalpixels = dimensions[0] * dimensions[1]
+print(totalpixels)
+pixpercent = "{:.0%}".format(colored / totalpixels )
+print(pixpercent)
+#	output = cv2.bitwise_and(image, image, mask = mask)
+
+	# show the images
+#	cv2.imshow("images", np.hstack([image, output]))
+#	cv2.waitKey(0)
